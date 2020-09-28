474d4ce184b499d2bb4e19eab6e1dd921a3e3667
ShobhitRatan <shobhit.ratan@baruchmail.cuny.edu>
2020-09-28 19:48:13 -0400

    Done.

--- lib/project_euler_power_digit_sum.rb
+++ lib/project_euler_power_digit_sum.rb
@@ -1,1 +1,7 @@
 # code your solution here
+def power_digit_sum(base, exponent) 
+    large_num = base**exponent 
+    large_num.to_s.split('').collect{|digit| digit.to_i}.inject(:+) 
+    puts large_num
+end 
+
